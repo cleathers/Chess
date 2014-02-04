c46d345e43f5dd52d91f3f1ea6df2059a6661a5a
App Academy Student <student@appacademy.io>
2014-02-04 15:03:47 -0800

    added game class

---
+++ Game.rb
@@ -0,0 +1,32 @@
+require './Board.rb'
+require './Pieces.rb'
+
+class Game
+  attr_reader :board
+  def initialize(filename = nil)
+
+    self.run
+  end
+
+  def run
+    @board = Board.new
+    self.place_pieces
+
+  end
+
+  def place_pieces
+    8.times do |index|
+      Pawn.new(:white, [6,index], @board)
+    end
+    8.times do |index|
+      Pawn.new(:black, [1,index], @board)
+    end
+    [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook].each_with_index do |piece, index|
+      piece.new(:white, [7, index], @board)
+    end
+    [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook].each_with_index do |piece, index|
+      piece.new(:black, [0, index], @board)
+    end
+  end
+
+end
